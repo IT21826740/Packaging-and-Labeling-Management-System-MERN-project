c90c077ee83ed3bec3f6e2225d1e73764ea214dc
IT21826740 <IT21826740@my.sliit.lk>
2023-11-02 17:01:08 +0530

    create complete orde model

---
+++ BACKEND/models/comOrder.js
@@ -0,0 +1,56 @@
+/*const mongoose = require('mongoose');
+
+const cmorderSchema = new mongoose.Schema({
+  orderID: {
+    type: String,
+    required: true,
+  },
+  weight: {
+    type: String,
+    required: true,
+  },
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  wrappingPaper: {
+    type: String,
+    required: true,
+  },
+  ribbonColor: {
+    type: String,
+    
+  },
+  customerWishes: {
+    type: String,
+    
+  },
+  qualityCheckerName:{
+    type: String,
+    required:true,
+  },
+  qualityCheckerID:{
+     type:String,
+     required:true,
+  },
+  checkDate:{
+    type: Date,
+    required: true,
+  },
+});
+
+const CMOrder = mongoose.model('Complete-Order', cmorderSchema);
+
+module.exports = CMOrder;*/
