bb92b86d2f3007d6a98c8bd38df9e53d439f8822
IT21826740 <IT21826740@my.sliit.lk>
2023-11-02 17:07:51 +0530

    delete

--- models/FileModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const fileSchema = new mongoose.Schema({
-  filename: String,
-  filePath: String,
-});
-
-module.exports = mongoose.model("Winners-File", fileSchema);
