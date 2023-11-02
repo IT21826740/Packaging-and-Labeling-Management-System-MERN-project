5f224aaebdc417f9c482651bb81b2e31707ca539
IT21826740 <IT21826740@my.sliit.lk>
2023-11-02 17:08:00 +0530

    delete

--- models/InvenoryFileModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const fileSchema = new mongoose.Schema({
-  filename: String,
-  filePath: String,
-});
-
-module.exports = mongoose.model("Inventory-File", fileSchema);
