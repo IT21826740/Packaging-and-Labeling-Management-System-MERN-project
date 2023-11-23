eae9fec3c6a7b9a8ded08deefc55ed6afc11dead
IT21826740 <IT21826740@my.sliit.lk>
2023-11-23 19:07:45 +0530

    change complete order routes

--- BACKEND/routes/comOrderRoutes.js
+++ BACKEND/routes/comOrderRoutes.js
@@ -2,42 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Order = require('../models/comOrder');
 
-/*
-// Create a new order
-router.post('/create', async (req, res) => {
-  try {
-    const {
-      orderID,
-      weight,
-      totalPrice,
-      date,
-      state,
-      type,
-      wrappingPaper,
-      ribbonColor,
-      customerWishes,
-    } = req.body;
 
-    const newOrder = new Order({
-      orderID,
-      weight,
-      totalPrice,
-      date,
-      state,
-      type,
-      wrappingPaper,
-      ribbonColor,
-      customerWishes,
-    });
-
-    await newOrder.save();
-    res.status(201).json(newOrder);
-  } catch (error) {
-    res.status(500).json({ error: 'Error creating the order' });
-  }
-});
-
-*/
 
 //read one
 router.route("/get/:id").get (async(req,res) =>{
